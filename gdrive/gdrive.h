54f9045add019d5e99baa9d3ccd64d070edc895a
BYVoid <byvoid.kcp@gmail.com>
2012-05-19 11:13:37 +0800

    Initial Commit

---
+++ gdrive/gdrive.h
@@ -0,0 +1,23 @@
+/*
+ *  gdrive.h
+ *  gdrive
+ *
+ *  Created by 郭 家寶 on 12/5/19.
+ *  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef gdrive_
+#define gdrive_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class gdrive
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
